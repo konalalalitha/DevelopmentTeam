6f77323dd310978514cfc4be3aa5e38f809c20f6
konalalalitha <58712718+konalalalitha@users.noreply.github.com>
2020-12-15 19:55:24 +0530

    Update my_user_config.h

--- tasmota/my_user_config.h
+++ tasmota/my_user_config.h
@@ -47,7 +47,7 @@
                                                  //  it only allows firmware upgrades starting from version 6.6.0.11
 
 // -- Project -------------------------------------
-#define PROJECT                "Smaragdine"         // PROJECT is used as the default topic delimiter
+#define PROJECT                "tasmota"         // PROJECT is used as the default topic delimiter
 
 // If not selected the default will be SONOFF_BASIC
 #define MODULE                 Smaragdine      // [Module] Select default module from tasmota_template.h
